f77f384488f6e06548e6b9054274029d09db4700
Gabriel Souza <gabriel_alves.14@hotmail.com>
2021-08-03 23:55:00 -0300

    first committ

---
+++ src/main/java/com/GADS/PersonAPI/PersonApiApplication.java
@@ -0,0 +1,15 @@
+package com.GADS.PersonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiApplication.class, args);
+	}
+
+
+
+}
