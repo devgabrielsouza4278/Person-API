52976eefa188e23b6b0a0eea6223cd0901a9c3fa
Gabriel Souza <gabriel_alves.14@hotmail.com>
2021-08-08 05:38:12 -0300

    Criação do método usando GET para listar uma única pessoa

---
+++ src/main/java/com/GADS/PersonAPI/Exception/PersonNotFoundException.java
@@ -0,0 +1,12 @@
+package com.GADS.PersonAPI.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PersonNotFoundException extends Exception {
+
+    public PersonNotFoundException(Long id) {
+        super("Pessoa não encontrada com o id " +id);
+    }
+}
