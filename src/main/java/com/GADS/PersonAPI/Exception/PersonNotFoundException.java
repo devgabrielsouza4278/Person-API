aef2c5d517164766ba76b97ed901238985c25d31
Gabriel Souza <gabriel_alves.14@hotmail.com>
2021-08-09 02:34:31 -0300

    Atualização no Lombok e criação do readme

--- src/main/java/com/GADS/PersonAPI/Exception/PersonNotFoundException.java
+++ src/main/java/com/GADS/PersonAPI/Exception/PersonNotFoundException.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class PersonNotFoundException extends Exception {
 
     public PersonNotFoundException(Long id) {
-        super("Pessoa não encontrada com o id " + id);
+        super(String.format("Person with ID %s not found!", id));
     }
 }
